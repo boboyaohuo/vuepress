c5e0f68ad99beb0cc1496d0ad23c62e8debfe9e3
吴建波 <wujianbo0429@163.com>
2020-01-20 11:21:44 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  base: "vuepress",
+  base: "/vuepress/",
   title: "你敲好看",
   description: "吴建波的博客",
-  head: [["link", { rel: "manifest", href: "/manifest.json" }]],
-  port: 8889
+  head: [["link", { rel: "icon", href: "image/favicon.ico" }]],
+  port: 8889,
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Haha", link: "/haha/" },
+      { text: "Github", link: "http://github.wujianbo.com" }
+    ]
+  },
+  extraWatchFiles: ['config.js']
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,10 @@
-# welcome to 你敲好看
+---
+home: true
+heroImage: /image/header.jpg
+heroText: 
+tagline: 吴建波的网络博客
+features:
+- title: 简洁
+  details: 以Markdown为中心的项目结构，以最少的配置帮助你专注于写作。
+footer: Copyright © 2020-present
+---
